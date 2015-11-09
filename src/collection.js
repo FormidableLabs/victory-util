717d30f0d944617d63637f73185e7178b4f4fe00
kstrack-grose <kiristrackgrose@gmail.com>
2015-11-09 13:54:16 -0800

    add methods to find the cumulative max and min over a dataset

--- src/collection.js
+++ src/collection.js
@@ -11,3 +11,19 @@ export const containsOnlyStrings = function (collection) {
     return _.isString(item);
   });
 };
+
+export const getCumulativeMax = function (datasets, dependentAxis) {
+  return _.reduce(datasets, (memo, dataset) => {
+    dataset = dataset.data || dataset;
+    const localMax = (_.max(_.pluck(dataset, dependentAxis)));
+    return localMax > 0 ? memo + localMax : memo;
+  }, 0);
+};
+
+export const getCumulativeMin = function (datasets, dependentAxis) {
+  return _.reduce(datasets, (memo, dataset) => {
+    dataset = dataset.data || dataset;
+    const localMin = (_.min(_.pluck(dataset, dependentAxis)));
+    return localMin < 0 ? memo + localMin : memo;
+  }, 0);
+};
