648877ab2e1769db540ebd6ccb21413e118ee113
Dale Bustad <dale@divmain.com>
2016-02-17 17:33:21 -0800

    Simplify style merging for cases where non-default style in unspecified.

--- src/helpers.js
+++ src/helpers.js
@@ -24,11 +24,14 @@ module.exports = {
     };
   },
 
-  getStyles(props, defaultStyles) {
-    const style = props.style || defaultStyles;
+  getStyles(style, defaultStyles, height, width) {
+    if (!style) {
+      return merge({}, defaultStyles, { parent: { height, width } });
+    }
+
     const {data, labels, parent} = style;
     return {
-      parent: merge({}, defaultStyles.parent, parent, {height: props.height, width: props.width}),
+      parent: merge({}, defaultStyles.parent, parent, { height, width }),
       labels: merge({}, defaultStyles.labels, labels),
       data: merge({}, defaultStyles.data, data)
     };
