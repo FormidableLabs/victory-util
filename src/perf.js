40093649464525cd426942216a33afc06091bc92
Dale Bustad <dale@divmain.com>
2016-02-17 16:54:27 -0800

    Add multi-argument memoization function.

---
+++ src/perf.js
@@ -0,0 +1,12 @@
+export function memoize (fn) {
+  const cache = {};
+  return function () {
+    const args = Array.prototype.slice.call(arguments);
+    const hash = args.map(arg => {
+      return (typeof arg === "string" || typeof arg === "number") ? arg : JSON.stringify(arg);
+    }).join("~");
+    return hash in cache ?
+      cache[hash] :
+      cache[hash] = fn.apply(this, args);
+  };
+}
